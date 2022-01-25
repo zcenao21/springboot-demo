be6d9edd54ff947ee80c2e57426d9cb9314a5981
zhangchao29 <zhangchao29@xiaomi.com>
2022-01-25 21:07:55 +0800

    test log4j

--- src/main/java/com/example/springbootdemo/controller/HelloController.java
+++ src/main/java/com/example/springbootdemo/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.springbootdemo.controller;
 
 import com.example.springbootdemo.service.HelloService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
+    private static final Logger logger = LogManager.getLogger();
+
     @Autowired
     private HelloService helloService;
 
@@ -16,5 +21,10 @@ public class HelloController {
     @ResponseBody
     public String helloWorld(){
         return helloService.sayHello();
+    }
+
+    public static void main(String[] args) {
+        String name = "${java:vm}";
+        logger.info("info:{}", name);
     }
 }
